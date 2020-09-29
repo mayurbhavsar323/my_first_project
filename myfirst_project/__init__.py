d9bfaa6d34005dd242e2579130032e940b393f70
mayurbhavsar323 <71587977+mayurbhavsar323@users.noreply.github.com>
2020-09-29 19:57:12 +0530

    add the new function

--- myfirst_project/__init__.py
+++ myfirst_project/__init__.py
@@ -4,3 +4,15 @@ def addition(a,b):
     "This FUNCTION addint the TWO NUMBERs"
     print(a+b)
 addition(2,4)    
+
+
+def f1():
+     print("Hello")
+f1()
+print(f1())
+
+##########  OUTPUT
+
+Hello
+Hello
+None
